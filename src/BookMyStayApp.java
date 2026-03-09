eb6d5d5013edbd155fd5fc044d57ca4e9dd0a1dc
SHERSHAM <shershamj@gmail.com>
2026-03-09 10:13:10 +0530

    Shersha Added UC1 Welcome Message Code

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,23 @@
+/**
+ * Use Case 1 - Application Entry & Welcome Message
+ * This class represents the entry point of the Book My Stay Application.
+ *
+ * @author Shersha
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("        BOOK MY STAY APP         ");
+        System.out.println("     Hotel Booking System        ");
+        System.out.println("          Version 1.0            ");
+        System.out.println("=================================");
+
+        System.out.println("Welcome to the Hotel Booking Application!");
+        System.out.println("Application started successfully.");
+
+    }
+}
